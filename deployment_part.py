d8d0eba29137c1823e906ead56a2efce0507f492
Pushpraj Gour <139035533+Pushpraj-Gour@users.noreply.github.com>
2024-02-26 13:06:34 +0530

    Update deployment_part.py

--- deployment_part.py
+++ deployment_part.py
@@ -27,8 +27,8 @@ def predictor(text):
     new_text= stemming(text)
     new_text = vector.transform([text])
     result = clr.predict(new_text)
-    if result == 0:
-        return f"'{text}'--> it's a negative Review"
+    if result == 4:
+        return f"'{text}'--> it's a Positive Review"
     else:
         return f"'{text}'--> It's a negative Review"
 
